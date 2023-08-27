10f329b1cf5e185f3bc7d48c8c74d5d5b2bc960e
Donildo <you@example.com>
2023-08-27 19:56:03 -0400

    main

---
+++ orgamo/src/componentes/Banner/index.js
@@ -0,0 +1,12 @@
+import './Banner.css'
+
+function Banner(){
+    return(
+        <header className="banner">
+           <img src="/img/banner.png" alt="banner principal da pagina Orgamo"/>
+        </header>
+        
+    )
+}
+
+export default Banner
